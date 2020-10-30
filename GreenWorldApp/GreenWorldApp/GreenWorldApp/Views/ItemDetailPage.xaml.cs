8e6cf007af487f4647fe72fd0e3b6885b3648c14
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-10-30 17:01:23 +1300

    Fixed files

---
+++ GreenWorldApp/GreenWorldApp/GreenWorldApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using GreenWorldApp.ViewModels;
+
+namespace GreenWorldApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
