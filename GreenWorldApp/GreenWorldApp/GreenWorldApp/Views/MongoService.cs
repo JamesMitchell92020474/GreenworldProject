0579fa17645f99571dc7e0a2a341d5cd302e028d
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-11-09 19:24:18 +1300

    Database code added, data not displaying

---
+++ GreenWorldApp/GreenWorldApp/GreenWorldApp/Views/MongoService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Security.Authentication;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver.Linq;
+
+namespace GreenWorldApp.Views
+{
+    public static class MongoService
+    {
+        static IMongoCollection<ProductItem> productItemsCollection;
+        readonly static string dbName = "IT6041Project";
+        readonly static string collectionName = "IT6041App_products";
+        static MongoClient client;
+
+        static IMongoCollection<ProductItem> ProductItemsCollection
+        {
+            get
+            {
+                if (client == null || productItemsCollection == null)
+                {
+                    var conx = "mongodb+srv://admin:admin@it6041project.mxz4f.mongodb.net/test?authSource=admin&replicaSet=atlas-zyx1o5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+                    MongoClientSettings settings = MongoClientSettings.FromUrl(
+                        new MongoUrl(conx)
+                    );
+
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
+
+                    client = new MongoClient(settings);
+                    var db = client.GetDatabase(dbName);
+
+                    var collectionSettings = new MongoCollectionSettings { ReadPreference = ReadPreference.Nearest };
+                    productItemsCollection = db.GetCollection<ProductItem>(collectionName, collectionSettings);
+                }
+
+                return productItemsCollection;
+            }
+        }
+
+        public async static Task<List<ProductItem>> GetAllItems()
+        {
+            var allItems = await productItemsCollection
+                .Find(new BsonDocument())
+                .ToListAsync();
+
+            return allItems;
+        }
+
+        public async static Task<List<ProductItem>> SearchByName(string name)
+        {
+            var results = await productItemsCollection
+                            .AsQueryable()
+                            .Where(tdi => tdi.Name.Contains(name))
+                            .Take(10)
+                            .ToListAsync();
+
+            return results;
+        }
+
+        public async static Task InsertItem(ProductItem item)
+        {
+            await productItemsCollection.InsertOneAsync(item);
+        }
+
+        public async static Task<bool> DeleteItem(ProductItem item)
+        {
+            var result = await productItemsCollection.DeleteOneAsync(tdi => tdi.Id == item.Id);
+
+            return result.IsAcknowledged && result.DeletedCount == 1;
+        }
+    }
+}
--- GreenWorldApp/GreenWorldApp/GreenWorldApp/Views/ProductPage.xaml.cs
+++ GreenWorldApp/GreenWorldApp/GreenWorldApp/Views/ProductPage.xaml.cs
@@ -1,20 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
 
 namespace GreenWorldApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProductPage : TabbedPage
+    public partial class ProductPage : ContentPage
     {
+        public ObservableCollection<ProductItem> ProductItems { get; set; }
+
         public ProductPage()
         {
             InitializeComponent();
+            ProductItems = new ObservableCollection<ProductItem>();
+
+            productList.ItemsSource = ProductItems;
+            Console.Out.WriteLine(ProductItems);
+        }
+
+        protected async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var allItems = await MongoService.GetAllItems();
+
+            //var allItems = await MongoService.SearchByName("first");
+
+            foreach (var item in allItems)
+            {
+                if (!ProductItems.Any((arg) => arg.Id == item.Id))
+                    ProductItems.Add(item);
+            }
+
+            if (allItems.Count == 0)
+            {
+                var newItem = new ProductItem { Name = "The first item", Description = "Long description that's boring" };
+                await MongoService.InsertItem(newItem);
+
+                ProductItems.Add(newItem);
+            }
+        }
+
+        protected async void Add_Clicked(object sender, EventArgs eventArgs)
+        {
+            var detailPage = new NavigationPage(new ProductDetailPage());
+
+            await Navigation.PushModalAsync(detailPage, true);
+        }
+
+        protected async void Delete_Item(object sender, EventArgs eventArgs)
+        {
+            if (!(sender is MenuItem menuItem))
+                return;
+
+            if (!(menuItem.CommandParameter is ProductItem ProductItem))
+                return;
+
+            var success = await MongoService.DeleteItem(ProductItem);
+
+            if (success)
+            {
+                ProductItems.Remove(ProductItem);
+            }
         }
     }
 }
