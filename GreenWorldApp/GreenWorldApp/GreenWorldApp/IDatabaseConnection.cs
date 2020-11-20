967619de5b5dc0d3d276ea5e501d356711014a3e
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-11-20 16:58:03 +1300

    SQLite database not working

---
+++ GreenWorldApp/GreenWorldApp/GreenWorldApp/IDatabaseConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenWorldApp
+{
+    public interface IDatabaseConnection
+    {
+        SQLite.SQLiteConnection DbConnection();
+    }
+}
