76bfc972955af04fa0bd85cc857d6fa77c4d0730
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-11-23 16:26:12 +1300

    Replaced with new project

---
+++ GWApp/GWApp/GWApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GWApp.ViewModels;
+using GWApp.Views;
+using Xamarin.Forms;
+
+namespace GWApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
