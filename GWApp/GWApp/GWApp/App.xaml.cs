b9086999459ebc12353277564d3d9ac269caeeb7
JamesMitchell92020474 <jamesmitchelldesign1@gmail.com>
2020-11-24 16:52:27 +1300

    New almost working app

---
+++ GWApp/GWApp/GWApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using GWApp.Views;
+
+namespace GWApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GWApp/GWApp/GWApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GWApp.ViewModels;
+using GWApp.Views;
+using Xamarin.Forms;
+
+[assembly: ExportFont("BebasNeue.ttf", Alias = "ThemeFont")]
+[assembly: ExportFont("Roboto.ttf", Alias = "LightFont")]
+namespace GWApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
